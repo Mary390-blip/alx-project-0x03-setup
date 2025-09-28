4ceda155b081245ee45f2f327cdb76ac82409325
Mary Hellen Otinga <maryotinga98@gmail.com>
2025-09-28 14:07:07 +0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,3 @@
-const Home = () => {
-  return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold">Welcome to Splash App</h1>
-    </div>
-  )
-}
-
-export default Home;
-
-const Home = () => {
-  return (
-    <div>
-      <h1> Welcome to Splash App </h1>
-    </div>
-  )
-}
 
 export default Home;
 
