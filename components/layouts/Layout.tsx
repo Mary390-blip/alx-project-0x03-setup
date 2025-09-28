4e6e9405054005e33f686620b13ba4d078917750
Mary Hellen Otinga <maryotinga98@gmail.com>
2025-09-28 13:36:09 +0400

    Update Layout.tsx

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -17,3 +17,19 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 };
 
 export default Layout;
+
+import { LayoutProps } from "@/interfaces";  // ✅ import here
+import Footer from "./Footer";
+import Header from "./Header";
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="pt-24">{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
